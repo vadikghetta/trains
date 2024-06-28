a9e3d8d42aecf5ee68d9551a7d839d671372ac6c
vadikghetta <122256298+vadikghetta@users.noreply.github.com>
2024-06-28 16:21:47 +0300

    add config

---
+++ config/loaders.ts
@@ -0,0 +1,87 @@
+import MiniCssExtractPlugin from "mini-css-extract-plugin";
+import { ModuleOptions } from "webpack";
+import { IWebpackOptions } from "../types/webpack.types";
+
+
+
+export function createLoaders  (options : IWebpackOptions) : ModuleOptions["rules"] {
+  const isDev = options.mode === "development" ? true : false;
+  const cssModules =   {
+    loader: "css-loader",
+    options: {
+      modules: {
+        localIdentName: isDev ? "[path][name]__[local]" : "[path][hash:base64:8]",
+      } 
+    },
+  }
+    const assetLoader =    {
+      test: /\.(png|jpg|gif)$/i,
+      use: [
+        {
+          loader: 'file-loader',
+          options: {
+            limit: 8192,
+          }
+        },
+      ],
+
+     type: 'javascript/auto'
+    }
+    const sassLoader =  {
+      test: /\.s[ac]ss$/i,
+      use: [
+        isDev ? "style-loader" : MiniCssExtractPlugin.loader,
+        cssModules,
+        "sass-loader",
+      ],
+    }
+
+    const svgrLoader = {
+      test: /\.svg$/i,
+      use: [
+          {
+              loader: '@svgr/webpack',
+              options: {
+                  icon: true,
+                  svgoConfig: {
+                      plugins: [
+                          {
+                              name: 'convertColors',
+                              params: {
+                                  currentColor: true,
+                              }
+                          }
+                      ]
+                  }
+              }
+          }
+      ],
+  }
+//   вместо бабеля и ts-loader решил попробовать swc loader
+  const swcLoader =  {
+      test: /\.tsx$/,
+      exclude: /(node_modules|bower_components)/,
+      use: {
+          loader: "swc-loader",
+          options: {
+              jsc: {
+                  parser: {
+                      syntax: "typescript"
+                  },
+                  transform : {
+                    react : {
+                      runtime : 'automatic'
+                    }
+                  }
+              }
+          }
+      }
+  
+  }
+    return [
+      sassLoader,
+      svgrLoader,
+      assetLoader,
+      swcLoader
+    ]
+}
