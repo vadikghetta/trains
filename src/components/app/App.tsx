758ceca16eaa92e96d8dbed46bda60821984e7f4
vadikghetta <122256298+vadikghetta@users.noreply.github.com>
2024-06-28 17:20:47 +0300

    testing styles and react

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -1,9 +1,15 @@
 import { useState } from "react";
+import styles from "./App.module.scss"
 
 const App = () => {
     const [state, setState] = useState(0)
     return (
-        <h1>Hello {state}</h1>
+        <div className={styles.main}>
+            <h1>Hello {state}</h1>
+            <button onClick={() => {
+                setState(state => state += 1)
+            }}>Click me</button>
+        </div>
     )
 }
 
