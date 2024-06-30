35a70fb54367a3ba267956902a2b2e703805cd36
vadikghetta <helloiamvadik@mail.ru>
2024-06-30 17:52:52 +0300

    fix : styles

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -1,24 +1,18 @@
-import { useState } from "react";
+import Heading from "../heading/Heading";
 import styles from "./App.module.scss"
-import { useAppDispatch, useAppSelector } from "@/redux/hooks";
-import { increment } from "@/redux/slices/counterSlice";
+
 
 
 const App = () => {
-    const [state, setState] = useState(0);
-    const counterState = useAppSelector(state => state.counter).value;
-    const dispatch = useAppDispatch();
+
     return (
-        <div className={styles.main}>
-            <h1>Hello {state}</h1>
-            <button onClick={() => {
-                setState(state => state += 1)
-            }}>Click me</button>
-            <h2>Hello from redux {counterState}</h2>
-            <button onClick={() => {
-                dispatch(increment())
-            }}>Click me for redux</button>
-        </div>
+        <main className={styles.main}>
+            <div className="container">
+                <Heading tag="h1" className={styles.title}>
+                    Поезда
+                </Heading>
+            </div>
+        </main>
     )
 }
 
--- src/components/heading/Heading.tsx
+++ src/components/heading/Heading.tsx
@@ -8,16 +8,19 @@ interface IHeadingProps extends DetailedHTMLProps<HTMLAttributes<HTMLHeadingElem
 }
 
 
-const Heading = ({ tag, children, ...props }: IHeadingProps) => {
+const Heading = ({ tag, children, className, ...props }: IHeadingProps) => {
     switch (tag) {
         case "h1": {
-            return <h1 className={clsx(styles.heading, styles.h1, props.className)} {...props}>{children}</h1>
+            return <h1 className={clsx(className, styles.heading, styles.h1)} {...props}>{children}</h1>
         }
         case "h2": {
-            return <h2 className={clsx(styles.heading, styles.h2, props.className)} {...props}>{children}</h2>
+            return <h2 className={clsx(className, styles.heading, styles.h2)} {...props}>{children}</h2>
         }
         default: {
             return <></>
         }
     }
 }
+
+
+export default Heading;
