b7596ffc4fa94fe8f97002aec513ae17f5344b20
vadikghetta <helloiamvadik@mail.ru>
2024-06-30 19:19:26 +0300

    add loader

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -4,35 +4,42 @@ import Heading from "../heading/Heading";
 import styles from "./App.module.scss"
 import { useEffect } from "react";
 import { fetchTrains } from "@/redux/slices/trainsSlice";
+import Loader from "../loader/Loader";
 
 
 
 const App = () => {
-    const trains = useAppSelector(state => state.trains);
+    const { trains: { data, loading } } = useAppSelector(state => state);
     const dispatch = useAppDispatch()
     useEffect(() => {
         dispatch(fetchTrains());
     }, [dispatch])
-    console.log(trains)
+
     return (
         <main className={styles.main}>
             <div className="container">
                 <Heading tag="h1" className={styles.title}>
                     Поезда
                 </Heading>
-                <div className={styles.inner}>
-                    <Box>
-                        <Heading tag="h2">
-                            Поезда
-                        </Heading>
-                    </Box>
-                    <Box>
-                        <Heading tag="h2">
-                            Характеристики
-                        </Heading>
-                    </Box>
-                </div>
+
+                {loading === "pending" ? <Loader /> : null}
+                {loading === "succeeded" ? (
+                    <div className={styles.inner}>
+                        <Box>
+                            <Heading tag="h2">
+                                Поезда
+                            </Heading>
+                        </Box>
+                        <Box>
+                            <Heading tag="h2">
+                                Характеристики
+                            </Heading>
+                        </Box>
+                    </div>
+                ) : null}
+                {loading === "failed" ? <Heading tag="h2">Произошла ошибка</Heading> : null}
             </div>
+
         </main>
     )
 }
